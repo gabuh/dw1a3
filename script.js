33fea0fd8378c8710c555197d70ec1e669481506
Gabuh <nogueira.santos@aluno.ifsp.edu.br>
2023-02-20 00:20:24 -0300

    activity event

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ function goPage(x){
   window.location.href = "https://gabuh.github.io/dw1a3/A"+ x ;
 }
 
-function scaleElement(el,y){
-  if (y == 0) {
+function zoomHere(el,y,x){
+  const items = document.getElementsByClassName("textContent");
+  if (y == true) {
     el.style.zoom=0;
+    items[x].style.display="none";
   }else{
     el.style.zoom=2;
+    items[x].style.display="block";
   }
 }
-
