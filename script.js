9d1e45e8d45f0fba7f62aa565abd9156f833d03e
gabuh <nogueira.santos@aluno.ifsp.edu.br>
2023-02-20 21:57:22 -0300

    small changes

--- script.js
+++ script.js
@@ -2,13 +2,25 @@ function goPage(x){
   window.location.href = "https://gabuh.github.io/dw1a3/A"+ x ;
 }
 
-function zoomHere(el,y,x){
-  const items = document.getElementsByClassName("textContent");
-  if (y == true) {
-    el.style.zoom=0;
-    items[x].style.display="none";
-  }else{
-    el.style.zoom=2;
-    items[x].style.display="block";
-  }
+function init(){
+  const element = document.getElementsByClassName("btn");
+  const contents = document.getElementsByClassName("textContent");
+  for(let i=0; i<element.length ;i++ ){
+    element.item(i).addEventListener("mouseover", function() {
+      this.style.zoom="2";
+      this.style.background="#F8F9FA";
+      this.style.color="#212529";
+      contents.item(i).style.display="block";
+    }); 
+    
+    element.item(i).addEventListener("mouseout", function(){
+      this.style.zoom="0";
+      this.style.background="#343A40";
+      this.style.color="#F8F9FA";
+      contents.item(i).style.display="none";
+    });
+  } 
+ 
 }
+
+document.addEventListener('DOMContentLoaded', init, false);
