82bfac0115e76a3cfd1c33a687bb0772b324b0f3
gabuh <nogueira.santos@aluno.ifsp.edu.br>
2023-03-06 01:46:29 -0300

    layout

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 function goPage(x){
   x>0?x="A"+x:x="HW";
    window.location.href = "https://gabuh.github.io/dw1a3/"+ x ;
+} 
+
+
+
+var id = null;
+function logoAnimation() {
+  const el = document.getElementById("logo-animated");
+  var pos = 1;
+  clearInterval(id);
+  id = setInterval(frame_logo, 380);
+  function frame_logo(){
+    if(pos == 14){
+      clearInterval(id);
+    }else{
+      el.src = `http://127.0.0.1:5500/assets/animation/${pos}.svg`;
+      pos++;
+    }
+  }
 }
 
 function init(){
@@ -9,19 +27,22 @@ function init(){
   for(let i=0; i<element.length ;i++ ){
     element.item(i).addEventListener("mouseover", function() {
       this.style.zoom="2";
-      this.style.background="#F8F9FA";
-      this.style.color="#212529";
+      this.style.background="#9d0eb8";
+      this.style.color="#F8F9FA";
       contents.item(i).style.display="block";
     }); 
     
     element.item(i).addEventListener("mouseout", function(){
       this.style.zoom="0";
-      this.style.background="#343A40";
-      this.style.color="#F8F9FA";
+      this.style.background="#d9fe05";
+      this.style.color="#000000";
       contents.item(i).style.display="none";
     });
   } 
- 
+
+  
+  document.getElementById("logo-animated").addEventListener("mouseover",logoAnimation);
+  logoAnimation();
 }
 
 document.addEventListener('DOMContentLoaded', init, false);
