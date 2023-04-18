36c1e711b1e9a2abc3387a9d41631b84595e5c9f
gabuh <nogueira.santos@aluno.ifsp.edu.br>
2023-04-17 23:03:57 -0300

    project scroll

---
+++ A3/script.js
@@ -0,0 +1,6 @@
+document.getElementById("modalInterface-left").addEventListener("click",function(){
+    document.getElementById("projectList").scrollTo(200-200,0)
+});
+document.getElementById("modalInterface-right").addEventListener("click",function(){
+    document.getElementById("projectList").scrollTo(200,0);
+});
